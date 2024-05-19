beca8fdb61510469b26e44c8614026aca4f1eb80
KateParf <katya.parfenova.2005@yandex.ru>
2024-05-19 19:10:53 +0400

    Алсу - управление видео

--- web/ClientApp/src/app/Pages/dance/dance.component.ts
+++ web/ClientApp/src/app/Pages/dance/dance.component.ts
@@ -1,10 +1,12 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, AfterViewInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from "@angular/router";
 import { MarkdownService } from 'ngx-markdown';
 import { DanceEpoch, DanceLevel, DanceType } from 'src/app/Models/models';
 import { Favorites } from '../favorites/favs.db';
 import { FavDance } from "../favorites/favorites.component";
+
+declare var videojs: any;
 
 @Component({
   selector: 'app-catalog',
@@ -18,27 +20,18 @@ export class DanceComponent {
   public danceLevel: string = "";
   public danceEpoch: string = "";
   public danceType: string = "";
-  public inFav: boolean = false;
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string, route: ActivatedRoute, private markdownService: MarkdownService, private favs: Favorites) {
+  constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string, route: ActivatedRoute, private markdownService: MarkdownService, private favs: Favorites) {
     void route.params.subscribe(params => this.danceId = params["id"]);
-
     this.getFav();
-    http.get<Dance>(baseUrl + 'api/dance/' + this.danceId).subscribe(result => {
+    this.loadDanceData();
+  }
+
+  private loadDanceData() {
+    this.http.get<Dance>(this.baseUrl + 'api/dance/' + this.danceId).subscribe(result => {
       this.dance = result;
 
-      // свой обработчик ссылок
-      this.markdownService.renderer.link = (href, title, text) => {
-        let prefix = text.substring(0, 3);
-        if (prefix == "vid") {
-          return `<strong><a [routerLink]="d" (click)="onPlayTime()" onClick="onPlayTime()" >(>) ${text.substring(4)}</a></strong>`;
-        } else
-          if (prefix == "mov") {
-            console.log(href);
-            return `<strong><a href="/move/${href}">${text.substring(4)}</a></strong>`;
-          } else
-            return `>>> <a href=${href}>${text}</a> <<<`;
-      };
+      this.setupMarkdownRenderer();
 
       this.danceScheme = this.markdownService.parse(this.dance.scheme);
       this.danceHistory = this.markdownService.parse(this.dance.history);
@@ -48,13 +41,68 @@ export class DanceComponent {
     }, error => console.error(error));
   }
 
+  private setupMarkdownRenderer() {
+    // свой обработчик ссылок
+    this.markdownService.renderer.link = (href, title, text) => {
+      let prefix = text.substring(0, 3);
+      if (prefix == "vid") {
+        return `<strong><a [routerLink]="d" (click)="onPlayTime()" onClick="onPlayTime()" >(>) ${text.substring(4)}</a></strong>`;
+      } else
+        if (prefix == "mov") {
+          console.log(href);
+          return `<strong><a href="/move/${href}">${text.substring(4)}</a></strong>`;
+        } else
+          return `>>> <a href=${href}>${text}</a> <<<`;
+    };    
+  }
+
+
   //-- player
+
+  private player: any;
+
+  public getPlayer() {
+    if (!this.player) {
+      this.player = videojs('player1');    
+      this.player.ready(() => {
+        console.log('Player is ready', this.player);
+      });
+    }
+    return this.player;
+  }
 
   public onPlayTime() {
     console.log("play time");
   }
 
+  // проигрывает отрезок видео по времени
+  playSegment(startMinutes: number, startSeconds: number, endMinutes: number, endSeconds: number) {
+    const startTime = startMinutes * 60 + startSeconds;
+    const endTime = endMinutes * 60 + endSeconds;
+    var loopEnabled = true;
+
+    let player = this.getPlayer();    
+    player.on('timeupdate', () => {
+        if (loopEnabled){
+            if (player.currentTime() < startTime || player.currentTime() >= endTime ) {
+              player.currentTime(startTime);
+            }
+        }
+    });
+
+    player.currentTime(startTime);
+    player.play();
+  }
+
+  setPlaybackRate(rate: number) {
+    this.getPlayer().playbackRate(rate);
+  }
+
+
   //-- favorites
+
+  public inFav: boolean = false;
+
   async getFav() {
     this.inFav = (await this.favs.findDance(this.danceId)) != undefined;
   }
